bd7d88524c1316ee12caef6f6d161763eb73ed7d
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2020-12-08 13:02:40 +0000

    Valid non-working framework for AURN imputation tests.
    The AurnImputationTest class is valid, and the imputation_test
    function can be run. But the data manipulation and data
    plotting functions are only placeholders at the moment.

--- environmental_data_modules/aurn_imputation_tests.py
+++ environmental_data_modules/aurn_imputation_tests.py
@@ -22,9 +22,9 @@ class AurnImputationTest(AurnPostProcessor):
     DEFAULT_DATA_LOSS_POSITION = 'end'
     
     
-    def __init__(self, metadata_filename=AurnModule.DEFAULT_METADATA_FILE, metadata_url=AurnModule.DEFAULT_METADATA_URL,
-                 out_dir=DEFAULT_OUT_DIR, verbose=PostProcessor.DEFAULT_VERBOSE):
-        """ Initialise instance of the AurnPostProcessor class.
+    def __init__(self, metadata_filename=AurnPostProcessor.DEFAULT_METADATA_FILE, metadata_url=AurnPostProcessor.DEFAULT_METADATA_URL,
+                 out_dir=AurnPostProcessor.DEFAULT_OUT_DIR, verbose=AurnPostProcessor.DEFAULT_VERBOSE):
+        """ Initialise instance of the AurnImputationTest class.
             Initialises the private class variables
 
             Args:
@@ -37,28 +37,41 @@ class AurnImputationTest(AurnPostProcessor):
                 Initialised instance of AurnPostProcessor
 
         """
-        super(AurnImputationTest, self).__init__(out_dir, verbose)
-        AurnModule.__init__(self, metadata_filename=metadata_filename, metadata_url=metadata_url)
-        DateRangeProcessor.__init__(self)
+        super(AurnImputationTest, self).__init__(metadata_filename,metadata_url,out_dir,verbose)
 
-        self._emep_data = None
-        self.min_years_reference = AurnImputationTest.DEFAULT_MIN_YEARS_REFERENCE
-        self.min_years = AurnImputationTest.DEFAULT_MIN_YEARS
-        self.impute_data = False
-        self._imputer = None
-        self._transformer = None
+        self._data_lost = AurnImputationTest.DEFAULT_DATA_LOST
+        self._data_loss_position = AurnImputationTest.DEFAULT_DATA_LOSS_POSITION
 
-
-
+    @property
+    def data_lost(self):
+        return self._data_lost
+    
+    @data_lost.setter
+    def data_lost(self, data_lost):
+        if type(data_lost) == float and 0 <= data_lost < 1:
+            self._data_lost = data_lost
+        else:
+            raise Exception('data_lost is {}, but should be float from 0 upto (not including) 1'.format(data_lost))
+            
+    @property
+    def data_loss_position(self):
+        return self._data_loss_position
+    
+    @data_loss_position.setter
+    def data_loss_position(self, data_loss_position):
+        if type(data_loss_position) == str and data_loss_position in ['start','middle','end','random']:
+            self._data_loss_position = data_loss_position
+        else:
+            raise Exception("data_loss_position is {}, but should be string matching one of these: ['start','middle','end','random']".format(data_loss_position))
 
     def imputation_test(self, in_file, date_range=None,
-                site_list=AurnModule.DEFAULT_SITE_LIST,
-                emep_filename=DEFAULT_EMEP_FILENAME,
-                min_years_reference=DEFAULT_MIN_YEARS_REFERENCE,
-                min_years=DEFAULT_MIN_YEARS,
+                site_list=AurnPostProcessor.DEFAULT_SITE_LIST,
+                emep_filename=AurnPostProcessor.DEFAULT_EMEP_FILENAME,
+                min_years_reference=AurnPostProcessor.DEFAULT_MIN_YEARS_REFERENCE,
+                min_years=AurnPostProcessor.DEFAULT_MIN_YEARS,
                 data_lost=DEFAULT_DATA_LOST, 
                 data_loss_position=DEFAULT_DATA_LOSS_POSITION,
-                save_to_csv=PostProcessor.DEFAULT_SAVE_TO_CSV,
+                save_to_csv=AurnPostProcessor.DEFAULT_SAVE_TO_CSV,
                 outfile_suffix=''):
 
         """ Testing the imputation methods used for filling in missing data. Replicates the
@@ -115,8 +128,8 @@ class AurnImputationTest(AurnPostProcessor):
 
         # Process inputs
         if date_range is not None:
-            self.date_range = [datetime.strptime(date_range[0], DateRangeProcessor.INPUT_DATE_FORMAT),
-                               datetime.strptime(date_range[1], DateRangeProcessor.INPUT_DATE_FORMAT)]
+            self.date_range = [datetime.strptime(date_range[0], AurnPostProcessor.INPUT_DATE_FORMAT),
+                               datetime.strptime(date_range[1], AurnPostProcessor.INPUT_DATE_FORMAT)]
         else:
             self.date_range = [self.get_available_start(), self.get_available_end()]
 
@@ -125,10 +138,11 @@ class AurnImputationTest(AurnPostProcessor):
         self.min_years = min_years
         self.min_years_reference = min_years_reference
         self.site_list = site_list
+        self.data_lost = data_lost
+        self.data_loss_position = data_loss_position
         self.station_data = self.metadata['AURN_metadata'][['site_id', 'latitude', 'longitude', 'site_name']]
         if self.verbose > 1: print('Station data: \n {}'.format(self.station_data))
 
-        self.impute_data = True
 
         # load and prepare the hourly dataset
         hourly_dataframe = self.load_aurn_data(in_file)
@@ -144,8 +158,8 @@ class AurnImputationTest(AurnPostProcessor):
             return
 
         # data preparation, to create the test dataset for imputation
-        hourly_test_dataframe, site_list_internal = self.data_preparation(hourly_dataframe_filtered,
-            data_lost,data_loss_position,site_list_internal,reference_sites)
+        hourly_test_dataframe, reference_sites = self.data_preparation(hourly_dataframe_filtered,
+            site_list_internal,reference_sites)
 
         print('imputation of data, returning hourly data')
         hourly_imputed_dataframe = self.organise_data_imputation(
@@ -168,7 +182,7 @@ class AurnImputationTest(AurnPostProcessor):
 
         #return daily_dataframe
 
-    def data_preparation(self, hourly_dataframe, data_lost, data_loss_position, site_list_internal, reference_sites):
+    def data_preparation(self, hourly_dataframe, site_list_internal, reference_sites):
         """
         Prepare test data for imputation, by removing the specified amount of data from the test sites.
         
@@ -190,4 +204,115 @@ class AurnImputationTest(AurnPostProcessor):
                             items: (list of strings) the siteID's for our reference sites for each `spc` 
             site_list_internal: (list, strings) a single list of required sites
         
+        Returns:
+            hourly_dataframe: hourly dataset, for all measurements, as pandas.Dataframe, with the required data removed
+                Index: none
+                Required Columns:
+                    Date   (datetime object):
+                    SiteID          (string):
+                Optional Columns:
+                    O3       (float):
+                    PM10     (float):
+                    PM2.5    (float):
+                    NO2      (float):
+                    NOXasNO2 (float):
+                    SO2      (float):
+            reference_sites: (dict, keys are species):
+                            items: (list of strings) the siteID's for our reference sites for each `spc`
+                                      These will have our test sites removed, to avoid issues later 
+        
         """
+        
+        
+        return hourly_dataframe, reference_sites
+
+    def imputation_hourly_analysis(self,hourly_imputed_dataframe,hourly_reference_dataframe,site_list_internal):
+        """
+            Statistical analysis of the hourly results for the imputation of AURN data
+        
+        Args:
+            hourly_imputed_dataframe: hourly dataset, for all measurements, as pandas.Dataframe
+                Index: none
+                Required Columns:
+                    Date   (datetime object):
+                    SiteID          (string):
+                Optional Columns:
+                    O3       (float):
+                    PM10     (float):
+                    PM2.5    (float):
+                    NO2      (float):
+                    NOXasNO2 (float):
+                    SO2      (float):
+            hourly_reference_dataframe: hourly dataset, for all measurements, as pandas.Dataframe
+                Index: none
+                Required Columns:
+                    Date   (datetime object):
+                    SiteID          (string):
+                Optional Columns:
+                    O3       (float):
+                    PM10     (float):
+                    PM2.5    (float):
+                    NO2      (float):
+                    NOXasNO2 (float):
+                    SO2      (float):
+            site_list_internal: (list, strings) a single list of required sites
+        
+        Returns: None
+        """
+        
+        print('...analysis of hourly imputed data is work in progress...')
+
+    def imputation_daily_analysis(self,daily_imputed_dataframe,daily_reference_dataframe,site_list_internal):
+        """
+            Statistical analysis of the daily results for the imputation of AURN data
+        
+        Args:
+            daily_imputed_dataframe: daily dataset, for all measurements, as pandas.Dataframe
+                Required MultiIndex:
+                    'time_stamp'  (datetime object): date (only) (e.g. 2017-06-01)
+                    'sensor_name'          (string): ID string for site (e.g. 'LIN3 [AQ]')
+                Required columns:
+                    'O3.max'       (float): daily maximum value
+                    'O3.mean'      (float): daily mean value
+                    'O3.flag'      (float): flag to indicate fraction of imputed data
+                                                    (1 = fully imputed, 0 = no imputed values were used)
+                    'PM10.max'       (float): daily maximum value
+                    'PM10.mean'      (float): daily mean value
+                    'PM10.flag'      (float): flag to indicate fraction of imputed data
+                                                    (1 = fully imputed, 0 = no imputed values were used)
+                    'PM2.5.max'       (float): daily maximum value
+                    'PM2.5.mean'      (float): daily mean value
+                    'PM2.5.flag'      (float): flag to indicate fraction of imputed data
+                                                    (1 = fully imputed, 0 = no imputed values were used)
+                    'NO2.max'       (float): daily maximum value
+                    'NO2.mean'      (float): daily mean value
+                    'NO2.flag'      (float): flag to indicate fraction of imputed data
+                                                    (1 = fully imputed, 0 = no imputed values were used)
+                    'NOXasNO2.max'       (float): daily maximum value
+                    'NOXasNO2.mean'      (float): daily mean value
+                    'NOXasNO2.flag'      (float): flag to indicate fraction of imputed data
+                                                    (1 = fully imputed, 0 = no imputed values were used)
+                    'SO2.max'       (float): daily maximum value
+                    'SO2.mean'      (float): daily mean value
+                    'SO2.flag'      (float): flag to indicate fraction of imputed data
+                                                    (1 = fully imputed, 0 = no imputed values were used)
+            daily_reference_dataframe: daily dataset, for all measurements, as pandas.Dataframe, 
+                          same layout as 'daily_imputed_dataframe'
+            site_list_internal: (list, strings) a single list of required sites
+        
+        Returns: None
+        """
+        
+        print('...analysis of daily mean/max imputed data is work in progress...')
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scripts/Data_Imputation_Testing/example_aurn_bash_script_impute.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+EMEP_FILE="emep_site_data_hourly_2016-2019.csv"
+META_DATA_URL="https://uk-air.defra.gov.uk/openair/R_data/AURN_metadata.RData"
+META_DATA_FILENAME="AURN_metadata.RData"
+SITES="ABD ABD7 ABD8 ARM6 AH"
+
+
+# EXAMPLE 1
+YEARS="2016 2017"
+ARGUMENTS_1=" --metadata_url ${META_DATA_URL} --metadata_filename ${META_DATA_FILENAME} --years ${YEARS} --sites ${SITES} --verbose 2"
+
+# EXAMPLE 2
+YEARS="2017"
+ARGUMENTS_2=" --metadata_url ${META_DATA_URL} --metadata_filename ${META_DATA_FILENAME} --years ${YEARS} --sites ${SITES} --verbose 2"
+
+
+
+
+# For Dougs test
+
+META_DATA_URL="https://uk-air.defra.gov.uk/openair/R_data/AURN_metadata.RData"
+META_DATA_FILENAME="AURN_metadata.RData"
+OUT_DIR="../AURN_Data_Download/test_Ann"
+SITES="BARN BAR2 BAR3 BIL BIR BRAD BDMA CHS6"
+MINYEARS='1'
+USEYEARS='1'
+VERBOSE=3
+DATE_RANGE="2016-01-01_00 2019-12-31_23"
+INFILE_SUFFIX='2016-2017'
+OUTFILE_SUFFIX='2016-2017_imputed'
+
+ARGUMENTS_1="--metadata_url ${META_DATA_URL} --date_range ${DATE_RANGE} --min_years ${MINYEARS} --min_years_ref ${USEYEARS} --metadata_filename ${META_DATA_FILENAME} --sites ${SITES} -o ${OUT_DIR} -s ${INFILE_SUFFIX} --verbose ${VERBOSE}"
+
+python AURN_impute_testing.py ${ARGUMENTS_1}
