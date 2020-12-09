5fdd1f896e17b39e2da13952cf3ba1d7da01c243
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2020-12-09 12:42:26 +0000

    Run script for AURN impute tests updated.

--- scripts/Data_Imputation_Testing/example_aurn_bash_script_impute.sh
+++ scripts/Data_Imputation_Testing/example_aurn_bash_script_impute.sh
@@ -24,15 +24,17 @@ META_DATA_FILENAME="AURN_metadata.RData"
 OUT_DIR="../AURN_Data_Download/test_Ann"
 #SITES="BARN BAR2 BAR3 BIL BIR BRAD BDMA CHS6"
 SITES="LEED MID MY1 PT4"
+SITES="LEED"
 MINYEARS='1'
 USEYEARS='3.8'
 VERBOSE=3
 DATE_RANGE="2016-01-01_00 2019-12-31_23"
 INFILE_SUFFIX='2016-2017'
 OUTFILE_SUFFIX='2016-2017_imputed'
-DATA_LOSS_POS='start' 
+DATA_LOSS_POS='middle'
+DATA_LOST='0.25'
 
-ARGUMENTS_1="--data_loss_position ${DATA_LOSS_POS} --metadata_url ${META_DATA_URL} --date_range ${DATE_RANGE} --min_years ${MINYEARS} --min_years_ref ${USEYEARS} --metadata_filename ${META_DATA_FILENAME} --sites ${SITES} -o ${OUT_DIR} -s ${INFILE_SUFFIX} --verbose ${VERBOSE}"
+ARGUMENTS_1="--data_loss_position ${DATA_LOSS_POS} --data_lost ${DATA_LOST} --metadata_url ${META_DATA_URL} --date_range ${DATE_RANGE} --min_years ${MINYEARS} --min_years_ref ${USEYEARS} --metadata_filename ${META_DATA_FILENAME} --sites ${SITES} -o ${OUT_DIR} -s ${INFILE_SUFFIX} --verbose ${VERBOSE}"
 
 #echo ${ARGUMENTS_1}
 
